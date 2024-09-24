536bbac518894037d127c2d52f0fe7f6e8106970
Dforce <kcy493@gmail.com>
2024-09-24 23:08:40 +0500

    Update index.php
    Fix Bugs

--- index.php
+++ index.php
@@ -15,3 +15,4 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
+"Fix: Resolved issue where contact form was not submitting due to missing nonce validation" && git push origin main
